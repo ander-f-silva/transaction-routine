96447b04a42e844b12eb8ed56770b0ab4e7066af
andferreira <anderson.ferreira@mercadolivre.com>
2020-11-06 11:18:36 -0300

    [Struct Project] Build struct the project

---
+++ src/main/java/br/com/pm/account/TransactionRoutineApplication.java
@@ -0,0 +1,13 @@
+package br.com.pm.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionRoutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pm/account/TransactionRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pm.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
