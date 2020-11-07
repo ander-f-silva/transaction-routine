8954112950c37bc2eb5139c1138f4512f6323b39
andferreira <anderson.ferreira@mercadolivre.com>
2020-11-06 23:30:20 -0300

    [Create Account] Add new rule to existing account

--- src/main/java/br/com/pm/account/domain/services/AccountService.java
+++ src/main/java/br/com/pm/account/domain/services/AccountService.java
@@ -2,6 +2,7 @@ package br.com.pm.account.domain.services;
 
 import br.com.pm.account.application.dto.AccountRequest;
 import br.com.pm.account.domain.repositories.AccountRepository;
+import br.com.pm.account.domain.services.exception.AccountAlreadyCreatedException;
 import br.com.pm.account.infrastructure.jpa.entities.AccountEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,13 @@ class AccountService implements AccountOperation {
 
     @Override
     public Long create(AccountRequest accountRequest) {
-        var accountEntity = accountRepository.save(new AccountEntity(accountRequest.getDocumentNumber()));
+        var documentNumber = accountRequest.getDocumentNumber();
+
+        if (accountRepository.existsByDocumentNumber(documentNumber)) {
+            throw new AccountAlreadyCreatedException("An account already exists with the data reported");
+        }
+
+        var accountEntity = accountRepository.save(new AccountEntity(documentNumber));
         return accountEntity.getId();
     }
 }
