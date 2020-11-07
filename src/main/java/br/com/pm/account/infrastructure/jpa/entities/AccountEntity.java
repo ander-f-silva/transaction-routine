43c827aad273f91bc1f3797dc148261867602816
andferreira <anderson.ferreira@mercadolivre.com>
2020-11-06 23:15:55 -0300

    [Create Account] Change name field document to document_number in entity

--- src/main/java/br/com/pm/account/infrastructure/jpa/entities/AccountEntity.java
+++ src/main/java/br/com/pm/account/infrastructure/jpa/entities/AccountEntity.java
@@ -1,15 +1,14 @@
 package br.com.pm.account.infrastructure.jpa.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "accounts")
 public class AccountEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "document_number", updatable = false, nullable = false)
     private String documentNumber;
 
     public AccountEntity() {
